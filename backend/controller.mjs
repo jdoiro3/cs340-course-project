dcba1fa96bea2211fdf50a69779b35d55026ea7b
jdoiro3 <josephdoiron1234@yahoo.com>
2022-05-12 16:22:28 -0400

    added swagger UI for API docs/testing

--- backend/controller.mjs
+++ backend/controller.mjs
@@ -6,6 +6,19 @@ import cors from 'cors'
 import { executeQuery } from './util.mjs'
 import fs from 'fs'
 import bodyParser from 'body-parser'
+import swaggerUi from 'swagger-ui-express'
+import swaggerJSDoc from 'swagger-jsdoc'
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Database Admin API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./controller.mjs']
+}
 
 // constants
 
@@ -20,6 +33,19 @@ app.use(cors())
 
 // endpoints
 
+// creates api docs and swagger UI for testing
+const swaggerSpec = swaggerJSDoc(options)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+/**
+ * @swagger
+ * /initialize:
+ *   get:
+ *     description: Initializes the database with starter data
+ *     responses:
+ *       200:
+ *         description: Returns SQL output as JSON object
+ */
 app.get(`/initialize`, async (req, res) => {
     fs.readFile('initialize.sql', 'utf8', async (err, sql) => {
         if (err) res.send(err);
@@ -28,12 +54,46 @@ app.get(`/initialize`, async (req, res) => {
     })
 })
 
+/**
+ * @swagger
+ * /tables:
+ *   get:
+ *     description: Get table names
+ *     responses:
+ *       200:
+ *         description: Returns an array of tables in the database
+ */
 app.get('/tables', async (req, res) => {
     let tables = await executeQuery(db.pool, 'SHOW TABLES;')
     tables.forEach((t, i) => tables[i] = t.Tables_in_cs340_doironj)
     res.status(200).type('json').json(tables)
 })
 
+
+/**
+ * @swagger
+ * /{table}:
+ *   get:
+ *     description: Retrieve a database table.
+ *     parameters:
+ *       - in: path
+ *         name: table
+ *         required: true
+ *         description: Table name in the database
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: filterBy
+ *         type: string
+ *         description: Column to filter the table by
+ *       - in: query
+ *         name: search
+ *         type: string
+ *         description: Value to filter the column by
+ *     responses:
+ *       200:
+ *         description: Returns JSON Object representing table
+ */
 app.get(`/:table`, async (req, res) => {
     let data
     if (req.query.filterBy !== undefined && req.query.search !== undefined) {
@@ -73,18 +133,89 @@ app.get('/tables/data', async (req, res) => {
     res.status(200).type('json').json(entities)
 })
 
+/**
+ * @swagger
+ * /{table}/{_id}:
+ *   post:
+ *     description: Insert a new record into a table.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *     parameters:
+ *       - in: path
+ *         name: table
+ *         required: true
+ *         description: Table name in the database
+ *         type: string
+ *       - in: path
+ *         name: _id
+ *         type: integer
+ *         description: Column to filter the table by
+ *     responses:
+ *       200:
+ *         description: Returns string
+ */
 app.post(`/:table/:_id`, jsonParser, async (req, res) => {
     let table = req.params.table
     let _id = req.params._id
-    let values = req.body // {column: value}
+    let valuesObj = req.body
+    let columns = Object.keys(valuesObj).map(c => String(c)).join(",")
+    let values = Object.values(valuesObj).map(v => {
+        if (typeof(v) === 'number') {
+            return String(v)
+        } else {
+            return `'${String(v)}'`
+        }
+    }).join(",")
+    let sqlStmt = `INSERT INTO ${table} (${columns}) VALUES (${values});`
+    res.json({_id, sql: sqlStmt})
     // insert new record into table
 })
 
-app.put(`/:table`, jsonParser, async (req, res) => {
+/**
+ * @swagger
+ * /{table}/{_id}:
+ *   put:
+ *     description: Update a record into a table.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *     parameters:
+ *       - in: path
+ *         name: table
+ *         required: true
+ *         description: Table name in the database
+ *         type: string
+ *       - in: path
+ *         name: _id
+ *         type: integer
+ *         description: Column to filter the table by
+ *     responses:
+ *       200:
+ *         description: Returns string
+ */
+app.put(`/:table/:_id`, jsonParser, async (req, res) => {
     let table = req.params.table
     let _id = req.params._id
-    let values = req.body // {column: value}
-    // update record in table
+    let valuesObj = req.body
+    let set = Object.keys(valuesObj).map(c => {
+        let v = valuesObj[c]
+        if (typeof(v) === 'number') {
+            v = String(v)
+        } else {
+            v = `'${String(v)}'`
+        }
+        return `${c} = ${v}`
+    }).join(",")
+    let sqlStmt = `UPDATE ${table} SET ${set} WHERE id = ${_id};`
+    res.json({_id, table, sql: sqlStmt})
+    // insert new record into table
 })
 
 app.delete(`/:table/:_id`, async (req, res) => {
